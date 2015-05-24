1e3abd5925d33bb02c0d59c26992896eb1c19f5a
greatyao <njustyw@gmail.com>
2015-05-24 22:01:13 +0800

    提示错误的时候不用snackbar而是使用简单的toast

--- app/src/main/java/com/yaoyumeng/v2ex/utils/MessageUtils.java
+++ app/src/main/java/com/yaoyumeng/v2ex/utils/MessageUtils.java
@@ -5,22 +5,17 @@ import android.content.Context;
 import android.view.Gravity;
 import android.widget.Toast;
 
-import com.github.mrengineer13.snackbar.SnackBar;
-import com.yaoyumeng.v2ex.R;
+import com.yaoyumeng.v2ex.Application;
 
 /**
  * Created by yw on 2015/5/5.
  */
 public class MessageUtils {
 
-    public static void showErrorMessage(Context cxt, String errorString){
-        Activity activity = (Activity)cxt;
-        if(activity != null)
-            new SnackBar.Builder(activity)
-                    .withMessage(errorString)
-                    .withActionMessageId(R.string.error_title)
-                    .withStyle(SnackBar.Style.ALERT)
-                    .withDuration(SnackBar.LONG_SNACK).show();
+    public static void showErrorMessage(Context cxt, String errorString) {
+        Activity activity = (Activity) cxt;
+        if (activity == null)
+            Toast.makeText(Application.getContext(), errorString, Toast.LENGTH_LONG).show();
         else
             Toast.makeText(cxt, errorString, Toast.LENGTH_LONG).show();
     }
