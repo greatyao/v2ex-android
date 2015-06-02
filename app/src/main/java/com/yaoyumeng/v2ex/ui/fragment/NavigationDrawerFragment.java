660ac394a4e017325f850c7e3ce8ae6772ff85d4
greatyao <njustyw@gmail.com>
2015-06-02 21:10:22 +0800

    bug fix

--- app/src/main/java/com/yaoyumeng/v2ex/ui/fragment/NavigationDrawerFragment.java
+++ app/src/main/java/com/yaoyumeng/v2ex/ui/fragment/NavigationDrawerFragment.java
@@ -325,7 +325,7 @@ public class NavigationDrawerFragment extends BaseFragment {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
+        if (mDrawerToggle != null && mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
