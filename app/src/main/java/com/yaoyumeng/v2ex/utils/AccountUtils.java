f33f2fde7be66f5c8acd06e9eeb791ca2c1ab263
greatyao <njustyw@gmail.com>
2015-06-13 17:27:05 +0800

    更新个人帐户接口

--- app/src/main/java/com/yaoyumeng/v2ex/utils/AccountUtils.java
+++ app/src/main/java/com/yaoyumeng/v2ex/utils/AccountUtils.java
@@ -5,9 +5,9 @@ import android.content.Context;
 import com.yaoyumeng.v2ex.Application;
 import com.yaoyumeng.v2ex.api.HttpRequestHandler;
 import com.yaoyumeng.v2ex.api.V2EXManager;
-import com.yaoyumeng.v2ex.model.MemberModel;
 import com.yaoyumeng.v2ex.model.NodeModel;
 import com.yaoyumeng.v2ex.model.PersistenceHelper;
+import com.yaoyumeng.v2ex.model.ProfileModel;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -21,38 +21,41 @@ public class AccountUtils {
 
     public static final int REQUEST_LOGIN = 0;
 
-    private static final String key_login_member = "logined@member";
+    private static final String key_login_member = "logined@profile";
     private static final String key_fav_nodes = "logined@fav_nodes";
 
     /**
      * 帐号登陆登出监听接口
      */
-    public static interface OnAccountListener{
+    public static interface OnAccountListener {
         abstract public void onLogout();
 
-        abstract public void onLogin(MemberModel member);
+        abstract public void onLogin(ProfileModel member);
     }
 
     private static HashSet<OnAccountListener> listeners = new HashSet<OnAccountListener>();
 
     /**
      * 注册登录接口
+     *
      * @param listener
      */
-    public static void registerAccountListener(OnAccountListener listener){
+    public static void registerAccountListener(OnAccountListener listener) {
         listeners.add(listener);
     }
 
     /**
      * 取消登录接口的注册
+     *
      * @param listener
      */
-    public static void unregisterAccountListener(OnAccountListener listener){
+    public static void unregisterAccountListener(OnAccountListener listener) {
         listeners.remove(listener);
     }
 
     /**
      * 用户是否已经登录
+     *
      * @param cxt
      * @return
      */
@@ -62,15 +65,16 @@ public class AccountUtils {
 
     /**
      * 保存登录用户资料
+     *
      * @param cxt
      * @param profile
      */
-    public static void writeLoginMember(Context cxt, MemberModel profile) {
+    public static void writeLoginMember(Context cxt, ProfileModel profile) {
         PersistenceHelper.saveModel(cxt, profile, key_login_member);
 
         //通知所有页面,登录成功,更新用户信息
         Iterator<OnAccountListener> iterator = listeners.iterator();
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             OnAccountListener listener = iterator.next();
             listener.onLogin(profile);
         }
@@ -78,15 +82,17 @@ public class AccountUtils {
 
     /**
      * 获取登录用户信息
+     *
      * @param cxt
      * @return
      */
-    public static MemberModel readLoginMember(Context cxt) {
+    public static ProfileModel readLoginMember(Context cxt) {
         return PersistenceHelper.loadModel(cxt, key_login_member);
     }
 
     /**
      * 删除登录用户资料
+     *
      * @param cxt
      */
     public static void removeLoginMember(Context cxt) {
@@ -96,18 +102,20 @@ public class AccountUtils {
 
     /**
      * 保存节点收藏信息
+     *
      * @param cxt
      * @param nodes
      */
     public static void writeFavoriteNodes(Context cxt, ArrayList<NodeModel> nodes) {
         PersistenceHelper.saveObject(cxt, nodes, key_fav_nodes);
-        for(NodeModel node : nodes){
+        for (NodeModel node : nodes) {
             Application.getDataSource().favoriteNode(node.name, true);
         }
     }
 
     /**
      * 获取收藏节点信息
+     *
      * @param cxt
      * @return
      */
@@ -118,6 +126,7 @@ public class AccountUtils {
 
     /**
      * 删除节点信息
+     *
      * @param cxt
      */
     public static void removeFavNodes(Context cxt) {
@@ -127,6 +136,7 @@ public class AccountUtils {
 
     /**
      * 清除所有用户相关资料
+     *
      * @param cxt
      */
     public static void removeAll(Context cxt) {
@@ -135,67 +145,63 @@ public class AccountUtils {
 
         //通知所有页面退出登录了,清除登录痕迹
         Iterator<OnAccountListener> iterator = listeners.iterator();
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             OnAccountListener listener = iterator.next();
             listener.onLogout();
         }
     }
 
-    public static interface OnAccountFavoriteNodesListener{
+    /**
+     * 刷新登陆用户资料
+     *
+     * @param cxt
+     */
+    public static void refreshProfile(final Context cxt) {
+        V2EXManager.getProfile(cxt, new HttpRequestHandler<ProfileModel>() {
+            @Override
+            public void onSuccess(ProfileModel data) {
+                writeLoginMember(cxt, data);
+            }
+
+            @Override
+            public void onSuccess(ProfileModel data, int totalPages, int currentPage) {
+                onSuccess(data, 1, 1);
+            }
+
+            @Override
+            public void onFailure(String error) {
+            }
+        }, true);
+    }
+
+    public static interface OnAccountFavoriteNodesListener {
         void onAccountFavoriteNodes(ArrayList<NodeModel> nodes);
     }
 
     /**
      * 刷新用户收藏节点
+     *
      * @param cxt
      * @param listener
      */
     public static void refreshFavoriteNodes(final Context cxt,
-                                            final OnAccountFavoriteNodesListener listener){
+                                            final OnAccountFavoriteNodesListener listener) {
         V2EXManager.getFavoriteNodes(cxt, new HttpRequestHandler<ArrayList<NodeModel>>() {
             @Override
             public void onSuccess(ArrayList<NodeModel> data) {
                 AccountUtils.writeFavoriteNodes(cxt, data);
-                if(listener != null)
+                if (listener != null)
                     listener.onAccountFavoriteNodes(data);
             }
 
             @Override
-            public void onSuccess(ArrayList<NodeModel> nodes, int totalPages, int currentPage){
+            public void onSuccess(ArrayList<NodeModel> nodes, int totalPages, int currentPage) {
 
             }
 
             @Override
             public void onFailure(String error) {
 
-            }
-        });
-    }
-
-    public static interface OnAccountNotificationCountListener{
-        public void onAccountNotificationCount(int count);
-    }
-
-    /**
-     * 刷新用户的未读提醒数量
-     * @param cxt
-     */
-    public static void refreshNotificationCount(Context cxt,
-                                            final OnAccountNotificationCountListener listener){
-        V2EXManager.getNotificationCount(cxt, new HttpRequestHandler<Integer>() {
-            @Override
-            public void onSuccess(Integer count) {
-                if(listener != null && count > 0)
-                    listener.onAccountNotificationCount(count);
-            }
-
-            @Override
-            public void onSuccess(Integer count, int totalPages, int currentPage){
-
-            }
-
-            @Override
-            public void onFailure(String error) {
             }
         });
     }
