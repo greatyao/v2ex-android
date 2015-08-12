7553628c96c87569af5aac8eb49d9c8651054833
greatyao <njustyw@gmail.com>
2015-08-12 20:52:38 +0800

    修改话题回复数的颜色以及节点的背景颜色和网页一致

--- app/src/main/java/com/yaoyumeng/v2ex/ui/adapter/TopicsAdapter.java
+++ app/src/main/java/com/yaoyumeng/v2ex/ui/adapter/TopicsAdapter.java
@@ -2,9 +2,13 @@ package com.yaoyumeng.v2ex.ui.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RoundRectShape;
 import android.os.Parcelable;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -85,7 +89,8 @@ public class TopicsAdapter extends RecyclerView.Adapter<TopicsAdapter.ViewHolder
         viewHolder.title.setText(topic.title);
 
         //这里设置已读未读颜色
-        viewHolder.title.setTextColor(Application.getDataSource().isTopicRead(topic.id) ?
+        boolean read = Application.getDataSource().isTopicRead(topic.id);
+        viewHolder.title.setTextColor( read ?
                 mContext.getResources().getColor(R.color.list_item_read) :
                 mContext.getResources().getColor(R.color.list_item_unread));
 
@@ -104,8 +109,22 @@ public class TopicsAdapter extends RecyclerView.Adapter<TopicsAdapter.ViewHolder
         });
 
         if (topic.replies > 0) {
+            int count_color = read ?
+                    mContext.getResources().getColor(R.color.topic_count_read) :
+                    mContext.getResources().getColor(R.color.topic_count_unread);
             viewHolder.replies.setVisibility(View.VISIBLE);
             viewHolder.replies.setText(String.valueOf(topic.replies));
+            viewHolder.replies.setBadgeBackgroundColor(count_color);
+
+            //由于上面的setBadgeBackgroundColor没起作用
+            //以下代码组合了BadgeView类的getDefaultBackground和show两个函数
+            Resources res = viewHolder.replies.getResources();
+            int px = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, res.getDisplayMetrics());
+            float[] outerR = new float[] {px, px, px, px, px, px, px, px};
+            RoundRectShape rr = new RoundRectShape(outerR, null, null);
+            ShapeDrawable drawable = new ShapeDrawable(rr);
+            drawable.getPaint().setColor(count_color);
+            viewHolder.replies.setBackgroundDrawable(drawable);
         } else {
             viewHolder.replies.setVisibility(View.INVISIBLE);
         }
